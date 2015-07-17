556426e3f4704e7d59b71050aa24263fedeb4b63
Georgios <georgios@georgios-VirtualBox.(none)>
2015-07-17 09:06:55 +0100

    Modified run.sh

--- bridge/run.sh
+++ bridge/run.sh
@@ -4,6 +4,6 @@ TOTAL=$(($ROWS*$COLS)) # Total number of processes
 
 # g++ test_bridge.cc bridge.cc
 
-if mpic++ -pthread -std=c++11 *.cc; then		# COMPILATION
+if mpic++ -pthread -std=c++11 main.cc System.cc Tile.cc Bridge.cc; then		# COMPILATION
 	mpiexec -np $TOTAL ./a.out $ROWS $COLS		# EXECUTION
 fi
